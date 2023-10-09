17704b00259fb0a81d93701fa86f1c461e1c4b82
sailordi <sailordi11@gmail.com>
2023-10-09 11:15:34 +0300

    Dart-intro: variables done

---
+++ 2 variables/variables.dart
@@ -0,0 +1,18 @@
+import 'dart:math' as math;
+
+void main() {
+  // Declare and initialize variables
+  Object obj = 'Object'; 
+  String planet = 'Earth';
+  int year = DateTime.now().year; 
+  bool lucky = false; 
+  //const double pi = double.parse(math.pi.toStringAsFixed(2) ); 
+  const double pi = 3.14; 
+
+  // Printing the variables
+  print('obj: $obj');
+  print('planet: $planet');
+  print('year: $year');
+  print('lucky: $lucky');
+  print('pi: $pi');
+}
